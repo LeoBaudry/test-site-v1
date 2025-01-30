1ef9173a0fc97b337f9d507d6b785ff4958dd5fe
LeoBaudry <leo.baudry@edu.univ-fcomte.fr>
2025-01-30 22:11:15 +0100

    test-site-proj-s2

---
+++ tailwind.config.mjs
@@ -0,0 +1,73 @@
+
+export default {
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    theme: {
+      extend: {
+        colors: {
+          background: '#F5F5F1',
+          asia: '#FF4B4B',
+          africa: '#4CAF50',
+          latino: '#E67E22',
+          europe: '#4A90E2',
+        }
+      },
+    },
+    plugins: [],
+  }
+
+
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: ["class"],
+    content: ["app/**/*.{ts,tsx}", "components/**/*.{ts,tsx}", "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    theme: {
+      extend: {
+        colors: {
+          border: "hsl(var(--border))",
+          input: "hsl(var(--input))",
+          ring: "hsl(var(--ring))",
+          background: "hsl(var(--background))",
+          foreground: "hsl(var(--foreground))",
+          primary: {
+            DEFAULT: "hsl(var(--primary))",
+            foreground: "hsl(var(--primary-foreground))",
+          },
+          secondary: {
+            DEFAULT: "hsl(var(--secondary))",
+            foreground: "hsl(var(--secondary-foreground))",
+          },
+          destructive: {
+            DEFAULT: "hsl(var(--destructive))",
+            foreground: "hsl(var(--destructive-foreground))",
+          },
+          muted: {
+            DEFAULT: "hsl(var(--muted))",
+            foreground: "hsl(var(--muted-foreground))",
+          },
+          accent: {
+            DEFAULT: "hsl(var(--accent))",
+            foreground: "hsl(var(--accent-foreground))",
+          },
+          popover: {
+            DEFAULT: "hsl(var(--popover))",
+            foreground: "hsl(var(--popover-foreground))",
+          },
+          card: {
+            DEFAULT: "hsl(var(--card))",
+            foreground: "hsl(var(--card-foreground))",
+          },
+          asia: '#FF4B4B',
+          africa: '#4CAF50',
+          latino: '#E67E22',
+          europe: '#4A90E2',
+        },
+        borderRadius: {
+          lg: "var(--radius)",
+          md: "calc(var(--radius) - 2px)",
+          sm: "calc(var(--radius) - 4px)",
+        },
+      },
+    },
+    plugins: [require("tailwindcss-animate")],
+  }
